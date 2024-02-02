da8b20f442b2ff102b3f171133c13c3b606cf193
tobitprince <prince.tobit@strathmore.edu>
2024-02-02 09:11:27 +0300

    Refactor Server class to remove unnecessary comments and improve code readability

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -5,10 +5,8 @@ from typing import Dict, List
 
 
 class Server:
-    """
-    Create server class to paginate
-    a database of popular baby names.
-    """
+    """Create server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -36,11 +34,11 @@ class Server:
             }
         return self.__indexed_dataset
 
-
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        Retrieve info about a page from a given 
-        index and with a specified size.
+        Retrieve info about a page.
+
+        From a given index and with a specified size.
         """
         data = self.indexed_dataset()
         assert index is not None and index >= 0 and index <= max(data.keys())
